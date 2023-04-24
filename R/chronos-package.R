5059d60f022f3fd89493c7e5488ed420df11afff
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2023-04-24 13:59:59 +0200

    Update package metadata

---
+++ R/chronos-package.R
@@ -0,0 +1,31 @@
+#' @details
+#'  \tabular{ll}{
+#'   **Package:** \tab chronos \cr
+#'   **Type:** \tab Package \cr
+#'   **Version:** \tab 0.0.0 \cr
+#'   **License:** \tab GPL-3 \cr
+#'  }
+#'
+#' @author
+#'  **Full list of authors and contributors** (alphabetic order):
+#'
+#'  \tabular{ll}{
+#'   Nicolas Frerebeau \tab *Université Bordeaux Montaigne, France* \cr
+#'   Brice Lebrun \tab *Université Bordeaux Montaigne, France* \cr
+#'  }
+#'
+#'  **Package maintainer**
+#'
+#'  Nicolas Frerebeau\cr
+#'  \email{nicolas.frerebeau@@u-bordeaux-montaigne.fr}
+#'
+#'  Archéosciences Bordeaux (UMR 6034)\cr
+#'  Maison de l'Archéologie\cr
+#'  Université Bordeaux Montaigne\cr
+#'  F-33607 Pessac cedex\cr
+#'  France
+#' @name chronos-package
+#' @aliases chronos
+#' @docType package
+#' @keywords internal
+"_PACKAGE"
