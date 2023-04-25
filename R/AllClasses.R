87a73fca8d9ca2046596ea72e417c9c55534d8ff
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2023-04-25 18:09:48 +0200

    Export S4 class

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -5,15 +5,18 @@ NULL
 #' TimeScale
 #'
 #' An S4 class to represent a time scale.
-#' @slot era_label A [`character`] string giving the abbreviated label of the
-#'  time scale.
-#' @slot era_name A [`character`] string giving the name of the time scale.
-#' @slot era_epoch A [`numeric`] value giving the epoch year from which years
-#'  are counted (in Gregorian astronomical years).
+#' @slot .Data A `numeric` [`matrix`] giving the the observed time-series
+#'  values.
+#' @slot era_label A [`character`] string specifying the abbreviated label of
+#'  the time scale.
+#' @slot era_name A [`character`] string specifying the name of the time scale.
+#' @slot era_epoch A [`numeric`] value specifying the epoch year from which
+#'  years are counted (in Gregorian astronomical years).
 #' @slot era_scale An [`integer`] specifying the number of years represented by
 #'  one unit. It should be a power of 10 (i.e. 1000 means ka).
 #' @slot era_direction An [`integer`] specifying if years are counted backwards
 #'  (`-1`) or forwards (`1`) from `epoch`.
+#' @slot era_unit A [`character`] string specifying the name of the year unit.
 #' @slot era_days A [`numeric`] vector giving the average length of the year in
 #'  solar days.
 #' @author N. Frerebeau
@@ -38,6 +41,20 @@ NULL
 #' TimeSeries
 #'
 #' An S4 class to represent time series.
+#' @slot .Data A `numeric` [`matrix`] giving the the observed time-series
+#'  values.
+#' @slot era_label A [`character`] string specifying the abbreviated label of
+#'  the time scale.
+#' @slot era_name A [`character`] string specifying the name of the time scale.
+#' @slot era_epoch A [`numeric`] value specifying the epoch year from which
+#'  years are counted (in Gregorian astronomical years).
+#' @slot era_scale An [`integer`] specifying the number of years represented by
+#'  one unit. It should be a power of 10 (i.e. 1000 means ka).
+#' @slot era_direction An [`integer`] specifying if years are counted backwards
+#'  (`-1`) or forwards (`1`) from `epoch`.
+#' @slot era_unit A [`character`] string specifying the name of the year unit.
+#' @slot era_days A [`numeric`] vector giving the average length of the year in
+#'  solar days.
 #' @slot time_labels A [`character`] string specifying the names of the time
 #'  series.
 #' @slot time_start A [`numeric`] value specifying the year of the
@@ -54,6 +71,7 @@ NULL
 #' @docType class
 #' @aliases TimeSeries-class
 #' @keywords internal
+#' @exportClass TimeSeries
 .TimeSeries <- setClass(
   Class = "TimeSeries",
   slots = c(
