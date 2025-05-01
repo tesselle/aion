6b3c3474a9e19ede710bc61275b4a660274adbc5
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2025-05-01 16:34:59 +0200

    Use a pre-defined font for snapshot tests

--- inst/tinytest/test_intervals.R
+++ inst/tinytest/test_intervals.R
@@ -38,11 +38,7 @@ expect_equivalent(
 # Plot =========================================================================
 if (at_home()) {
   using("tinysnapshot")
-  options(tinysnapshot_device = "svglite")
-  options(tinysnapshot_height = 7) # inches
-  options(tinysnapshot_width = 7)
-  options(tinysnapshot_tol = 200) # pixels
-  options(tinysnapshot_os = "Linux")
+  source("helpers.R")
 
   plot_interval_rd <- function() plot(x, calendar = NULL)
   expect_snapshot_plot(plot_interval_rd, "plot_interval_rd")
--- inst/tinytest/test_plot.R
+++ inst/tinytest/test_plot.R
@@ -1,10 +1,6 @@
 if (at_home()) {
   using("tinysnapshot")
-  options(tinysnapshot_device = "svglite")
-  options(tinysnapshot_height = 7) # inches
-  options(tinysnapshot_width = 7)
-  options(tinysnapshot_tol = 200) # pixels
-  options(tinysnapshot_os = "Linux")
+  source("helpers.R")
 
   Sys.setenv(LANGUAGE = "en") # Force locale
 
